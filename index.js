a1c870bd724f7f238cd8f7b51116f7a099befab6
Abhi-Sony-PC <iabhisharma12@gmail.com>
2017-12-10 03:00:42 +0530

    added GET, POST, PUT, DELETE for products

--- index.js
+++ index.js
@@ -25,3 +25,125 @@ server
 
   // Maps req.body to req.params so there is no switching between them
   .use(restify.bodyParser())
+
+  
+// Get all products in the system
+server.get('/products', function (req, res, next) {
+  
+    // Find every entity within the given collection
+    productsSave.find({}, function (error, products) {
+  
+      // Return all of the products in the system
+      res.send(products)
+    })
+  })
+  
+  // Get a single product by their product id
+  server.get('/products/:id', function (req, res, next) {
+  
+    // Find a single product by their id within save
+    productsSave.findOne({ _id: req.params.id }, function (error, product) {
+  
+      // If there are any errors, pass them to next in the correct format
+      if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+  
+      if (product) {
+        // Send the product if no issues
+        res.send(product)
+      } else {
+        // Send 404 header if the product doesn't exist
+        res.send(404)
+      }
+    })
+  })
+  
+  // Create a new product
+  server.post('/products', function (req, res, next) {
+  
+    // Make sure name is defined
+    if (req.params.name === undefined ) {
+      // If there are any errors, pass them to next in the correct format
+      return next(new restify.InvalidArgumentError('name must be supplied'))
+    }
+    if (req.params.brand === undefined ) {
+      // If there are any errors, pass them to next in the correct format
+      return next(new restify.InvalidArgumentError('age must be supplied'))
+    }
+    var newProduct = {
+      name: req.params.name, 
+      brand: req.params.brand
+    }
+  
+    // Create the product using the persistence engine
+    productsSave.create( newProduct, function (error, product) {
+  
+      // If there are any errors, pass them to next in the correct format
+      if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+  
+      // Send the product if no issues
+      res.send(201, product)
+    })
+  })
+  
+  // Update a product by their id
+  server.put('/products/:id', function (req, res, next) {
+  
+    // Make sure name is defined
+    if (req.params.name === undefined ) {
+      // If there are any errors, pass them to next in the correct format
+      return next(new restify.InvalidArgumentError('name must be supplied'))
+    }
+    if (req.params.brand === undefined ) {
+      // If there are any errors, pass them to next in the correct format
+      return next(new restify.InvalidArgumentError('age must be supplied'))
+    }
+    
+    var newProduct = {
+      _id: req.params.id,
+      name: req.params.name, 
+      brand: req.params.brand
+    }
+    
+    // Update the product with the persistence engine
+    productsSave.update(newProduct, function (error, product) {
+  
+      // If there are any errors, pass them to next in the correct format
+      if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+  
+      // Send a 200 OK response
+      res.send(200)
+    })
+  })
+  
+  // Delete product with the given id
+  server.del('/products/:id', function (req, res, next) {
+  
+    // Delete the product with the persistence engine
+  productsSave.delete(req.params.id, function (error, product) {
+  
+      // If there are any errors, pass them to next in the correct format
+      if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+  
+      // Send a 200 OK response
+      res.send()
+    })
+  
+  })
+  
+  //Delete all products
+  server.del('/products', function(req,res,next){
+    
+    productsSave.delete(any,function(error,products){
+    
+    if(error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+    
+    res.send()
+    
+    })
+    
+    })
+  
+  
+  
+  
+  
